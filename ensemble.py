28bd0c103c2589e3a4e81a56771c317b4caf8e10
yanghuanye <yanghuanye@tencent.com>
2024-11-04 16:17:53 +0800

    FEAT: add retry for selector, add schema retrieval post processing

--- ensemble.py
+++ ensemble.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import argparse
 import json
+import os
 from func_timeout import func_timeout, FunctionTimedOut
 from sqlagent.utils import parse_sql
 from sqlagent.postprocessing.self_consistency import aggregate_sqls
@@ -9,6 +10,26 @@ from sqlagent.runner.database_manager import DatabaseManager
 from sqlagent.database_utils.db_info import get_db_schema
 
 
+def merge_retrieval_result(retrieval_result_list):
+    retrieval_result = {"similar_values": {}, "similar_columns": {}}
+    for retrieval_result_i in retrieval_result_list:
+        if not retrieval_result_i:
+            continue
+        for table, column_values in retrieval_result_i['similar_values'].items():
+            for column, values in column_values.items():
+                similar_values = retrieval_result['similar_values'].get(table, {}).get(column, [])
+                similar_values += values
+                similar_values = sorted(set(similar_values))
+                if table not in retrieval_result['similar_values']:
+                    retrieval_result['similar_values'][table] = {}
+                retrieval_result['similar_values'][table][column] = similar_values
+        for table, columns in retrieval_result_i['similar_columns'].items():
+            similar_columns = retrieval_result['similar_columns'].get(table, [])
+            similar_columns += columns
+            similar_columns = sorted(set(similar_columns))
+            retrieval_result['similar_columns'][table] = similar_columns
+            
+    return retrieval_result
 
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser()
@@ -47,8 +68,7 @@ if __name__ == '__main__':
 
         ensemble_model_id = "+".join([i.replace("/", "_") for i in ensemble_model])
         output_file = f"{emsemble_output_path}/{ensemble_method}_{ensemble_model_id}_pred_sql.json"
-        with open(output_file, 'w') as f:
-            f.write("")
+
         meta_time_out = 30
 
         count_steps = defaultdict(int)
@@ -100,69 +120,75 @@ if __name__ == '__main__':
         ensemble_model_id = "+".join([i.replace("/", "_") for i in ensemble_model])
         output_file = f"{emsemble_output_path}/{ensemble_method}_{selector_model}/{ensemble_method}_{ensemble_model_id}_pred_sql.json"
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
-        with open(output_file, 'w') as f:
-            f.write("")
+
+        pass_ids = []
+        if os.path.exists(output_file):
+            with open(output_file, 'r') as f:
+                for line in f:
+                    if line == '\n':
+                        continue
+                    example = json.loads(line)
+                    pass_ids.append(example["question_id"])
+        failed_ids = [i for i in range(len(eval_data)) if i not in pass_ids]
+        
         meta_time_out = 30
-        for question_id, example in enumerate(eval_data):
-            db_id = example["db_id"]
-            question = example["question"]
-            db_mode = "dev"
-            predictions = model_preds.get(question_id, {})
-            sqls = [parse_sql(v["response"]) for v in predictions.values()]
+        while failed_ids:
+            print(f"Failed to process {len(failed_ids)} questions: {failed_ids}")
+            for question_id, example in enumerate(eval_data):
+                if question_id in pass_ids:
+                    continue
+                db_id = example["db_id"]
+                question = example["question"]
+                db_mode = "dev"
+                predictions = model_preds.get(question_id, {})
+                sqls = [parse_sql(v["response"]) for v in predictions.values()]
 
-            retrieval_result = {"similar_values": {}, "similar_columns": {}}
-            for pred in predictions.values():
-                retrieval_result_i = pred["retrieval_result"]
-                # if "steps" in pred:
-                #     retrieval_result_i = pred["steps"][-1]["retrieval_result"]
-                # else:
-                #     retrieval_result_i = pred["retrieval_result"]
-                for table, column_values in retrieval_result_i['similar_values'].items():
-                    for column, values in column_values.items():
-                        similar_values = retrieval_result['similar_values'].get(table, {}).get(column, [])
-                        similar_values += values
-                        similar_values = sorted(set(similar_values))
-                        if table not in retrieval_result['similar_values']:
-                            retrieval_result['similar_values'][table] = {}
-                        retrieval_result['similar_values'][table][column] = similar_values
-                for table, columns in retrieval_result_i['similar_columns'].items():
-                    similar_columns = retrieval_result['similar_columns'].get(table, [])
-                    similar_columns += columns
-                    similar_columns = sorted(set(similar_columns))
-                    retrieval_result['similar_columns'][table] = similar_columns
-            
-            schema_with_examples = retrieval_result["similar_values"]
-            db_manager = DatabaseManager(db_mode=db_mode, db_id=db_id)
-            schema_string = db_manager.get_database_schema_string(
-                tentative_schema=get_db_schema(db_manager.db_path), 
-                schema_with_examples=schema_with_examples, 
-                schema_with_descriptions=None, 
-                include_value_description=True
-            )
-            error = ""
-            try:
-                final_sql = func_timeout(
-                    meta_time_out, 
-                    selector, 
-                    args=(sqls, question, schema_string, db_mode, db_id, selector_model, selector_temperature)
+                retrieval_result_list = []
+                for pred in predictions.values():
+                    # retrieval_result_i = pred["retrieval_result"]
+                    if "retrieval_result" not in pred and pred["steps"]:
+                        retrieval_result_i = pred["steps"][-1]["retrieval_result"]
+                    elif "retrieval_result" in pred:
+                        retrieval_result_i = pred["retrieval_result"]
+                    else:
+                        retrieval_result_i = {}
+                    retrieval_result_list.append(retrieval_result_i)
+                retrieval_result = merge_retrieval_result(retrieval_result_list)
+                
+                schema_with_examples = retrieval_result["similar_values"]
+                db_manager = DatabaseManager(db_mode=db_mode, db_id=db_id)
+                schema_string = db_manager.get_database_schema_string(
+                    tentative_schema=get_db_schema(db_manager.db_path), 
+                    schema_with_examples=schema_with_examples, 
+                    schema_with_descriptions=None, 
+                    include_value_description=True
                 )
-            except FunctionTimedOut:
-                error = "Timeout"
-                print(f"Timeout in {question_id}")
-                final_sql = sqls[0]
-            except Exception as e:
-                error = str(e)
-                print(f"Error in {question_id}: {error}")
-                final_sql = sqls[0]
+                error = ""
+                try:
+                    final_sql = func_timeout(
+                        meta_time_out, 
+                        selector, 
+                        args=(sqls, question, schema_string, db_mode, db_id, selector_model, selector_temperature)
+                    )
+                except FunctionTimedOut:
+                    error = "Timeout"
+                    print(f"Timeout in {question_id}")
+                    final_sql = sqls[0]
+                except Exception as e:
+                    error = str(e)
+                    print(f"Error in {question_id}: {error}")
+                    final_sql = sqls[0]
 
 
-            example["question_id"] = question_id
-            example["pred"] = {
-                "response": final_sql,
-                "ensemble_predictions": predictions,
-                "error": error
-            }
-            
-            with open(output_file, 'a') as f:
-                f.write(json.dumps(example) + "\n")
-        print(f"Ensemble results have been saved to {output_file}")
+                example["question_id"] = question_id
+                example["pred"] = {
+                    "response": final_sql,
+                    "ensemble_predictions": predictions,
+                    "error": error
+                }
+                pass_ids.append(question_id)
+                failed_ids = [i for i in range(len(eval_data)) if i not in pass_ids]
+                
+                with open(output_file, 'a') as f:
+                    f.write(json.dumps(example) + "\n")
+            print(f"Ensemble results have been saved to {output_file}")
--- post_process.py
+++ post_process.py
@@ -8,26 +8,69 @@ if __name__ == "__main__":
     args_parser = argparse.ArgumentParser()
     args_parser.add_argument('--result_file', type=str)
     args_parser.add_argument('--output_file', type=str)
+    args_parser.add_argument('--mode', type=str, default="sql", choices=["sql", "retrieval_result"])
+    
 
     args = args_parser.parse_args()
     result_file = args.result_file
     output_file = args.output_file
+    mode = args.mode
 
-    results = {}
-    with open(args.result_file, 'r') as f:
-        for line in f:
-            if line == '\n':
-                continue
-            example = json.loads(line)
-            prediction = example["pred"]["response"]
-            db_id = example["db_id"]
-            question_id = example["question_id"]
-            if question_id not in results:
-                results[question_id] = (prediction, db_id)
-                
-    for question_id, (prediction, db_id) in results.items():
-        sql = parse_sql(prediction)
-        results[question_id] = sql + '\t----- bird -----\t' + db_id
+    if mode == "sql":
 
-    with open(output_file, 'w') as f:
-        json.dump(results, f, indent=4)
+        results = {}
+        with open(args.result_file, 'r') as f:
+            for line in f:
+                if line == '\n':
+                    continue
+                example = json.loads(line)
+                prediction = example["pred"]["response"]
+                db_id = example["db_id"]
+                question_id = example["question_id"]
+                if question_id not in results:
+                    results[question_id] = (prediction, db_id)
+                    
+        for question_id, (prediction, db_id) in results.items():
+            sql = parse_sql(prediction)
+            results[question_id] = sql + '\t----- bird -----\t' + db_id
+
+        with open(output_file, 'w') as f:
+            json.dump(results, f, indent=4)
+    
+    elif mode == "retrieval_result":
+
+        results = {}
+        with open(args.result_file, 'r') as f:
+            for line in f:
+                if line == '\n':
+                    continue
+                example = json.loads(line)
+                question_id = example["question_id"]
+                gt_sql = example["SQL"]
+                if "pred" in example:
+                    pred = example["pred"]
+                    if "retrieval_result" not in pred and "steps" in pred:
+                        retrieval_result = pred["steps"][-1]["retrieval_result"]
+                    elif "retrieval_result" in pred:
+                        retrieval_result = pred["retrieval_result"]
+                    else:
+                        raise ValueError("No retrieval result found")
+                else:
+                    raise ValueError("No prediction found")
+                if question_id not in results:
+                    results[question_id] = (retrieval_result, gt_sql)
+                    
+        with open(output_file, 'w') as f:
+            f.write("")
+        
+        results = sorted(results.items(), key=lambda x: int(x[0]))
+
+        with open(output_file, 'a') as f:
+            for question_id, (retrieval_result, gt_sql) in results:
+                item = {
+                    "question_id": question_id,
+                    "SQL": gt_sql,
+                    "retrieval_result": retrieval_result,
+                    }
+                f.write(json.dumps(item) + '\n')
+
